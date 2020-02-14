f10b0798a49a3ff67843138b57539c0fa1b5619c
jpm <jpmckenna90@gmail.com>
2020-02-13 20:26:04 -0500

    THanks Quincy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const db = require("../express-note-taker/Develop/db/db.json");
+const db = require("./Develop/db/db.json");
 
 // Set up express app
 var app = express();
